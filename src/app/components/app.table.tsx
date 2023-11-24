c3cbf38b07c1977f55dffa862cdc23252d7fd6cc
Maduro29 <manhdungtran.vnuuet@gmail.com>
2023-11-24 11:30:45 +0700

    delete author commit

--- src/app/components/app.table.tsx
+++ src/app/components/app.table.tsx
@@ -5,10 +5,28 @@ import CreateModal from './create.modal';
 import { useState } from 'react';
 import UpdateModal from './update.modal';
 import Link from 'next/link';
+import axios from 'axios';
+import { mutate } from 'swr';
+import { toast } from 'react-toastify';
 
 interface IProps {
     authors: IAuthor[]
 }
+
+const deleteAuthor = async (idAuthor: number) => {
+    const response = await axios({
+        method: 'delete',
+        url: 'http://34.87.168.245:3000/deleteauthor',
+        data: {
+            idAuthor: idAuthor
+        },
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    });
+    mutate('http://34.87.168.245:3000/getallauthors');
+    return response.data;
+};
 
 function TestTable(props: IProps) {
 
@@ -18,6 +36,16 @@ function TestTable(props: IProps) {
 
     const { authors } = props;
     // console.log('check props:', authors)
+
+    const handleDelete = async (idAuthor: number) => {
+        const confirmation = window.confirm(`Bạn muốn xóa tác giả này chứ? id = ${idAuthor}`)
+        if (confirmation) {
+            const res = await deleteAuthor(idAuthor);
+            console.log(res)
+        } else {
+            toast.error('Bạn đã hủy xóa')
+        }
+    }
 
     return (
         <>
@@ -48,13 +76,19 @@ function TestTable(props: IProps) {
                                     }}>
                                         Edit
                                     </Button>
-                                    <Button className='mx-4' variant='danger'>Delete</Button>
+                                    <Button className='mx-4' variant='danger' onClick={
+                                        () => {
+                                            handleDelete(authorItem.id);
+                                        }
+                                    }>
+                                        Delete
+                                    </Button>
                                 </td>
                             </tr>
                         )
                     })}
                 </tbody>
-            </Table>
+            </Table >
             <CreateModal showModal={showModal} setShowModal={setShowModal} />
             <UpdateModal showModal={showModalUpdate} setShowModal={setShowModalUpdate} author={author} setAuthor={setAuthor} />
         </>
